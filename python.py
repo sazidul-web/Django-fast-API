28780b5ee4cd0c049a0443ab24e90049cad726ea
Sazidul <rabbiking00@gmail.com>
2025-02-01 02:06:08 +0600

    post mathod and secma validation

--- python.py
+++ python.py
@@ -3,9 +3,19 @@
 # local server newar command ---- uvicorn python:app ///// uvicorn + file name + app name
 # local server newar command ---- uvicorn python:app --reload ///// uvicorn + file name + app name aita dile api reload nibe.
 
-from fastapi import FastAPI
+from typing import Optional # for rating api
+from fastapi import FastAPI # for get api
+from fastapi.params import Body # for post api
+from pydantic import BaseModel # for post api validation
 
 app = FastAPI()
+
+class Post(BaseModel):
+    title:str
+    body:str
+    publis:bool = False
+    rating: Optional[int]=None
+
 
 @app.get("/")
 async def root():
@@ -14,3 +24,17 @@ async def root():
 @app.get("/post")
 async def root_post():
     return {"data":"Hello i am sazidul"}
+# post api data print hosse .........................
+@app.post("/create")
+async def create(payload: dict = Body(...)):
+    print(payload)
+    return {"title": payload["title"],
+            "body": payload["body"],
+        }
+
+@app.post("/validation")
+async def create(New_post:Post):
+    print(New_post.rating)
+    return {"data":"new post"
+        }
+
