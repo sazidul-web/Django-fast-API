af9cd839fb88eed8468accd09e1f255252f2c5df
Sazidul <rabbiking00@gmail.com>
2025-02-03 18:15:40 +0600

    change status code

--- python.py
+++ python.py
@@ -3,8 +3,9 @@
 # local server newar command ---- uvicorn python:app ///// uvicorn + file name + app name
 # local server newar command ---- uvicorn python:app --reload ///// uvicorn + file name + app name aita dile api reload nibe.
 
-from typing import Optional # for rating api
-from fastapi import FastAPI # for get api
+from typing import Optional
+from urllib import response # for rating api
+from fastapi import FastAPI , Response, status, HTTPException  # for get api
 from fastapi.params import Body # for post api
 from pydantic import BaseModel # for post api validation
 from random import randrange # for CARD api
@@ -31,7 +32,7 @@ async def mypost():
     return {"data": my_list}
 
 # curd post api new id create hoiya data base a sob alada vabe save thake........!
-@app.post("/curd")
+@app.post("/curd",status_code=status.HTTP_201_CREATED)
 async def curdpost(post: Post):
     post_dict=post.dict()
     post_dict['id']=randrange(0,100000000)
@@ -47,10 +48,14 @@ def get_latest_post():
 
 # get post by id ============================================
 @app.get("/mypost/{id}")
-def mypost(id: int):
+def mypost(id: int,response: Response):
     print(type(id))
     Post=find_post(id)
     print(type(id))
+    if not mypost:
+        HTTPException(status_code=status.HTTP)
+    # if mypost:
+    #     response.status_code=201
     return {"post_details": Post}
 
 @app.get("/")
